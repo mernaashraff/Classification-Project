ad2979d6cfafd582c372fde4e4e9d84d7133170c
mernaashraff <101405748+mernaashraff@users.noreply.github.com>
2023-09-23 18:26:55 +0300

    Your commit message here

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:07bd29b9d7481998ba4ad8df085a1e00f991523cf37a999502515a9b5324ac45
+size 1528
---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c8932c58b46ae9849e7808ea50898083fbef48ca03f6a09341e83ddea45f021
+size 5540
